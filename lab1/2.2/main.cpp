94eb0e84b1b1433e4bd8cfec3cc3535132eb14ce
JohnVonNeumann <louiswillcock@gmail.com>
2021-03-16 17:02:13 +1100

    UPDATE 2.2 - calculate area of circle

--- lab1/2.2/main.cpp
+++ lab1/2.2/main.cpp
@@ -1,7 +1,16 @@
 #include <cmath>
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << M_PI << std::endl;
+    float area = 0;
+    float radius;
+    std::cout << "Input radius of circle:\n";
+    std::cin >> radius;
+    // M_PI sourced from cmath
+    // area = pi * radius^2
+    area = M_PI * pow(radius, 2);
+    std::cout << "Area of circle: " << area;
     return 0;
 }
