c5e644fcb2fe58b44937c8d2f6980434ff4a5c4b
JohnVonNeumann <louiswillcock@gmail.com>
2021-03-30 22:16:22 +1100

    UPDATE main.cpp - add comments for code

--- lab2/3/main.cpp
+++ lab2/3/main.cpp
@@ -15,8 +15,17 @@
  */
 
 int main() {
+
+    // set POWER as a const as we only use one power in the equation and it is ugly enough
+    // without even more numbers
     const double POWER = 2.0;
 
+    // use uint16's here to move to statically sized types that can handle larger inputs
+    // than just the test inputs, I wanted to play around with types here, originally ran
+    // uint8_t but it turns out they're actually typedefs to char which caused some issues
+    // a solution was found (see git history) but it involved a scanf which I was a bit
+    // wary of due to a lack of err handling and input validation, which is outside the scope
+    // of this solution
     uint16_t F;
     uint16_t R;
     float C;
@@ -27,11 +36,17 @@ int main() {
     std::cout << "Please input your values in a space delimited string in the format of:" << std::endl
               << "Frequency Resistance Capacitance Inductance EMF" << std::endl;
 
+    // in reality in the future i'd swap this out for something more robust with err handling
+    // and input validation
     std::cin >> F >> R >> C >> L >> E;
 
+    // this is ugly, if I was gonna spend more time on this program, i'd split the equation into
+    // separate statements and refer to them by the parts of the equation using proper maths terms
+    // ... but I'm not
     I = E / sqrt(pow(R, POWER) + pow(((2 * M_PI * F * L) - ( 1 / (2 * M_PI * F * C))), POWER));
 
     std::cout << "Current: " <<  I << std::endl;
 
+    // probably shouldnt just exit 0
     return 0;
 }
