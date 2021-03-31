9c091e1541354a4daf3edc00e708456cc21563e8
JohnVonNeumann <louiswillcock@gmail.com>
2021-03-31 15:11:37 +1100

    CREATE 7/main.cpp - absolutely useless program

---
+++ lab3/7/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// if someone can explain the value of this task, I'd be appreciative
+int main() {
+    int num1 = 1;
+    int num2 = 2;
+    num1 = 3;
+    num2 = num1;
+
+    return 0;
+}
