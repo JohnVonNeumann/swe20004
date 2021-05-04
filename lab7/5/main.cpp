4f03465b972d561857809354759ec7487cbbe738
JohnVonNeumann <louiswillcock@gmail.com>
2021-05-04 16:49:41 +1000

    UPDATE main.cpp - add ret for s_func and add create_tutor func

--- lab7/5/main.cpp
+++ lab7/5/main.cpp
@@ -29,6 +29,17 @@ student_t create_student() {
     student.course_details = "Computer Science";
     student.units = {"Databases 101", "Operating Systems", "Networking", "Security"};
     student.grades = {"HD", "HD", "P", "F"};
+    return student;
+}
+
+tutor_t create_tutor() {
+    tutor_t tutor;
+    tutor.name = "John Von Neumann";
+    tutor.staff_id = 1337;
+    tutor.course_details = "Department of Everything";
+    tutor.units = {"Building the Bomb for Noobs", "Quantum Physics for Housewives", "Martini Making 101", "How to be a Lord"};
+    tutor.class_timetable = {"Tuesday 10am", "Wednesday 3pm", "Thursday 5pm", "Friday 8am"};
+    return tutor;
 }
 
 int main() {
