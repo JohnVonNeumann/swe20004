82fda502724c06e16df21521d3750760f2436734
JohnVonNeumann <louiswillcock@gmail.com>
2021-05-05 18:45:05 +1000

    UPDATE main.cpp - complete program with sorting method

--- lab6/5/main.cpp
+++ lab6/5/main.cpp
@@ -1,26 +1,61 @@
+// Program takes in input from the user and then sorts and calculates the mode
+// of the char array, finding the most common character input to the user
+
 #include <iostream>
 #include <array>
 
 const int MAXINPUT = 20;
+
+void sort_array(int count, std::array<char, MAXINPUT> chars) {
+
+    std::cout << "The original array is:" << std::endl;
+    for (int i = 0; i < count; ++i) {
+        std::cout << chars[i];
+    }
+
+    std::sort(std::begin(chars), std::end(chars));
+
+    std::cout << "\nThe sorted array is:" << std::endl;
+    for (int i = 0; i < count; ++i) {
+        std::cout << chars[i];
+    }
+
+}
+
+void calculate_mode(int size, std::array<char, MAXINPUT> chars) {
+
+    int counter = 1;
+    int max = 0;
+    char mode = chars[0];
+    for (int i = 0; i < size - 1; i++)
+    {
+        if ( chars[i] == chars[i+1] )
+        {
+            counter++;
+            if ( counter > max )
+            {
+                max = counter;
+                mode = chars[i];
+            }
+        } else
+            counter = 1;
+    }
+    std::cout << "\nThe mode is " << mode << " and it occurs " << counter << " times." << std::endl;
+}
 
 int main() {
     std::array<char,MAXINPUT> characters = {};
     int count = 0;
     std::cout << "Enter 20 characters of the alphabet without repeating: " << std::endl;
     while (count != 20) {
-        bool char_check = false;
         char input;
         std::cin >> input;
         if (isalpha(input)) {
             for (int i = 0; i < count; ++i) {
                 if(characters[i] == input) {
                     std::cout << input << " is a duplicate letter" << std::endl;
-                    char_check = true;
                     break;
                 }
-            }
-            if (char_check) {
-                continue;
             }
             characters[count] = input;
             count += 1;
@@ -29,5 +64,9 @@ int main() {
             continue;
         }
     }
+
+    sort_array(MAXINPUT, characters);
+    calculate_mode(MAXINPUT, characters);
     return 0;
 }
+
